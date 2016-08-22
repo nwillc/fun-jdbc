bf56e80bee2cb042bf2b7a12c6a839e4df5c6b16
Nicholas Christopher <nwillc@gmail.com>
2016-08-22 17:15:38 -0400

    partial

--- src/main/java/com/github/nwillc/funjdbc/utils/Copier.java
+++ src/main/java/com/github/nwillc/funjdbc/utils/Copier.java
@@ -3,24 +3,10 @@ package com.github.nwillc.funjdbc.utils;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 public class Copier {
-	public static <T> void copy(Consumer<T> setter, Function<Integer,T> getter, Integer index) {
-		setter.accept(getter.apply(index));
-	}
 
 	public static <T,B> void copy2(List<T> strings, B bean, BiConsumer<B,T> setter, BiFunction<List<T>, Integer,T> getter, Integer index) {
 		setter.accept(bean,getter.apply(strings,index));
-	}
-
-	public static void multi(Function<Integer,String> function, Integer ... keys){
-		Stream.of(keys).forEach(k -> System.out.println(function.apply(k)));
-	}
-
-	public static <T> void println(Function<T,String> function, T value) {
-		System.out.println(function.apply(value));
 	}
 }
--- src/test/java/com/github/nwillc/funjdbc/utils/CopierTest.java
+++ src/test/java/com/github/nwillc/funjdbc/utils/CopierTest.java
@@ -4,39 +4,22 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  *
  */
+
 public class CopierTest {
 	private List<String> strings = Arrays.asList("zero", "one", "two", "three");
 
-	@Test
-	public void shouldPrint() throws Exception {
-		BiFunction<List<String>, Integer, String> accessor = List::get;
-		System.out.println(accessor.apply(strings,0));
-	}
-
-	@Test
-	public void shouldCopy() throws Exception {
-		Bean bean = new Bean();
-		wrap(bean, strings);
-		assertThat(bean.str).isEqualTo(strings.get(1));
-	}
 
 	@Test
 	public void shouldCopy2() throws Exception {
 		Bean bean = new Bean();
 		Copier.copy2(strings, bean, Bean::setStr, List::get, 0);
 		assertThat(bean.str).isEqualTo(strings.get(0));
-	}
-
-	private void wrap(Bean b, List<String> l) {
-		Copier.copy(b::setStr, l::get, 1);
 	}
 
 
