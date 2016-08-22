984b3184312fbdca4ef34ca113affbfda8847db4
Nicholas Christopher <nwillc@gmail.com>
2016-08-22 14:05:39 -0400

    partial

--- src/main/java/com/github/nwillc/funjdbc/utils/Copier.java
+++ src/main/java/com/github/nwillc/funjdbc/utils/Copier.java
@@ -1,10 +1,26 @@
 package com.github.nwillc.funjdbc.utils;
 
+import java.util.List;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 public class Copier {
 	public static <T> void copy(Consumer<T> setter, Function<Integer,T> getter, Integer index) {
 		setter.accept(getter.apply(index));
+	}
+
+	public static void multi(Function<Integer,String> function, Integer ... keys){
+		Stream.of(keys).forEach(k -> System.out.println(function.apply(k)));
+	}
+
+	public static <T> T access(Function<Integer, T> function, Integer key) {
+		return function.apply(key);
+	}
+
+	public static <T> void println(Function<T,String> function, T value) {
+		System.out.println(function.apply(value));
 	}
 }
--- src/test/java/com/github/nwillc/funjdbc/utils/CopierTest.java
+++ src/test/java/com/github/nwillc/funjdbc/utils/CopierTest.java
@@ -3,7 +3,10 @@ package com.github.nwillc.funjdbc.utils;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -11,22 +14,29 @@ import static org.assertj.core.api.Assertions.assertThat;
  *
  */
 public class CopierTest {
+	private List<String> strings = Arrays.asList("zero", "one", "two", "three");
+
+	@Test
+	public void shouldPrint() throws Exception {
+		Copier.multi(integer -> strings.get(integer), 0, 3);
+	}
+
 	@Test
 	public void shouldCopy() throws Exception {
-		List<String> strings = new ArrayList<>();
-		strings.add("zero");
-		strings.add("one");
-
 		Bean bean = new Bean();
-		Copier.copy(bean::setNumber, strings::get, 1);
-		assertThat(bean.number).isEqualTo(strings.get(1));
+		wrap(bean, strings);
+		assertThat(bean.str).isEqualTo(strings.get(1));
+	}
+
+	private void wrap(Bean b, List<String> l) {
+		Copier.copy(b::setStr, l::get, 1);
 	}
 
 	private class Bean {
-		String number;
+		String str;
 
-		public void setNumber(String number) {
-			this.number = number;
+		public void setStr(String str) {
+			this.str = str;
 		}
 	}
 }
