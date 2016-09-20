5fe441bb03d37faed32c945761088d434440a6db
Nicholas Christopher <nwillc@Nicholass-MacBook-Pro.local>
2016-09-19 22:20:00 -0400

    deprecated cleaning

--- src/test/java/com/github/nwillc/funjdbc/ResultSetIteratorTest.java
+++ src/test/java/com/github/nwillc/funjdbc/ResultSetIteratorTest.java
@@ -39,7 +39,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import static com.github.nwillc.funjdbc.utils.Closer.close;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 public class ResultSetIteratorTest extends IteratorContract {
