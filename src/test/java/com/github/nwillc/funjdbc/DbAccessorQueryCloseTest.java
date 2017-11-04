21d0842d4b2a682cd323265a31319fdf32575318
Nicholas Christopher <nwillc@gmail.com>
2017-11-04 15:19:32 -0400

    Testing exception

---
+++ src/test/java/com/github/nwillc/funjdbc/DbAccessorQueryCloseTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2017, nwillc@gmail.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
+package com.github.nwillc.funjdbc;
+
+import com.github.nwillc.funjdbc.utils.Closer;
+import mockit.Expectations;
+import mockit.integration.junit4.JMockit;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@RunWith(JMockit.class)
+public class DbAccessorQueryCloseTest {
+    private InMemWordsDatabase dao;
+
+    @Before
+    public void setUp() throws Exception {
+        dao = new InMemWordsDatabase();
+        dao.create();
+    }
+
+    @Test
+    public void testExceptionCloser() throws Exception {
+        new Expectations(Closer.class){{
+            Closer.close((AutoCloseable)any); times = 3;
+        }};
+        assertThatThrownBy(() -> dao.dbQuery(null,null)).isInstanceOf(SQLException.class);
+    }
+}
